44b1439e0e248e77e1845b5c35ce94f0e7592bd9
Andy Bradbury <andy@thestartupguys.co.uk>
2017-07-02 22:27:05 +0100

    Implement dynamic form with calculation based on inputs

--- js/calculate.js
+++ js/calculate.js
@@ -1,27 +1,26 @@
 $( document ).ready(function() {
-  var websiteUniqueVisits = 100;
-  var websiteConversionPercent = 3;
-  var closeRatePercent = 50;
-  var sessionsPerWeek = 2;
-  var perSessionAmount = 30;
+  $('#roiForm').submit(function(e) {
+    e.preventDefault();
 
-  function calculateROI() {
-    // Calculate website leads by using our website conversion percent by the number of visits the website gets
+    // Grab values from form inputs
+    var websiteUniqueVisits = $('input[name="website-uniques"]').val(),
+        websiteConversionPercent = 3,
+        closeRatePercent = $('input[name="close-rate"]').val(),
+        sessionsPerWeek = $('input[name="sessions-weekly"]').val(),
+        perSessionAmount = $('input[name="per-session-value"]').val();
+
     var websiteLeads = websiteUniqueVisits * (websiteConversionPercent / 100);
     console.log("Website Leads: "+websiteLeads);
 
-    // Calculate closed sales by using the website leads and their close rate
     var closedSales = Math.ceil(websiteLeads * (closeRatePercent / 100));
     console.log("Closed Sales: "+closedSales);
 
     var customerValue = Math.round((sessionsPerWeek * 4) * perSessionAmount);
     console.log("Average Value of Customer: "+customerValue);
 
-    // Calculate amount of revenue per month from website by session amount in £ by number of sessions per week
-    return Math.ceil(closedSales * customerValue);
-
-  }
-  console.log(calculateROI());
+    var monthlyWebsiteRevenue = Math.ceil(closedSales * customerValue);
+    $('#result').text("£"+monthlyWebsiteRevenue+" in new sales, per month, from your website");
+  });
 });
 
 // TODO: Your website will pay for itself in X months/weeks
