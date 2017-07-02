8e9947960f27364ffc7ebe670c0d0b82c9a234a2
Andy Bradbury <andy@thestartupguys.co.uk>
2017-07-02 22:53:34 +0100

    Add basic form styling, refactor the function and add calculation text

--- js/calculate.js
+++ js/calculate.js
@@ -9,18 +9,22 @@ $( document ).ready(function() {
         sessionsPerWeek = $('input[name="sessions-weekly"]').val(),
         perSessionAmount = $('input[name="per-session-value"]').val();
 
-    var websiteLeads = websiteUniqueVisits * (websiteConversionPercent / 100);
-    console.log("Website Leads: "+websiteLeads);
+    // Do math to calculate revenue
+    var websiteLeads = websiteUniqueVisits * (websiteConversionPercent / 100),
+        closedSales = Math.ceil(websiteLeads * (closeRatePercent / 100)),
+        customerValue = Math.round((sessionsPerWeek * 4) * perSessionAmount),
+        monthlyWebsiteRevenue = Math.ceil(closedSales * customerValue);
 
-    var closedSales = Math.ceil(websiteLeads * (closeRatePercent / 100));
-    console.log("Closed Sales: "+closedSales);
+    // Show results and our calculations
+    $('#result').text("£"+monthlyWebsiteRevenue+" in new sales, per month, from your website.");
 
-    var customerValue = Math.round((sessionsPerWeek * 4) * perSessionAmount);
-    console.log("Average Value of Customer: "+customerValue);
-
-    var monthlyWebsiteRevenue = Math.ceil(closedSales * customerValue);
-    $('#result').text("£"+monthlyWebsiteRevenue+" in new sales, per month, from your website");
+    $('#calculations').html("You should get "+websiteLeads+" good leads from your website."+'<br />'+
+                            "Based on your close rate, you should turn "+closedSales+" of those into paying customers."+'<br />'+
+                            "Each customer is worth £"+customerValue+" per month, meaning you'll make around £"+monthlyWebsiteRevenue+" from the "+closedSales+" you sell to each month.");
   });
 });
 
 // TODO: Your website will pay for itself in X months/weeks
+// TODO: Leads from website doesn't mean consultation. Need to factor in how many show up for a consult.
+
+// Nice ref for basic, functional calculator: https://gist.github.com/michaelaguiar/1989078
